09e2e86fc8ee5ed17f17bbe8d630d82a9a9f0265
marius goller <miron@goller24.de>
2024-12-12 23:02:02 +0100

    Programm geschrieben

--- Aufgabe4_TextAnalyse/TextAnalyse/Program.cs
+++ Aufgabe4_TextAnalyse/TextAnalyse/Program.cs
@@ -1,29 +1,67 @@
-﻿static string LongestWord(string[] words,string longestWord) //gets all words in a Line and the prev longest word, returns longest word so far
+﻿
+static string LongestWord(string[] words, string longestWord) //gets all words in a Line and the prev longest word, returns longest word so far
 {
     string longest = longestWord;
-    for(int i = 0; i<words.Length;i++)
+    for (int i = 0; i < words.Length; i++)
     {
-        if(words[i].Length > longest.Length)
+        if (words[i].Length > longest.Length)
             longest = words[i];
     }
-    return longest; 
+    return longest;
 }
-StreamReader sr = new StreamReader(@"..\Test.txt");
-string line,longestWord = "";
-int symbolcount = 0,wordcount = 0,linecount = 0;
-while(!sr.EndOfStream)
+static int Index(char c)
+{
+    // Kleinbuchstaben (ohne Umlaute)
+    if (c >= 'a' && c <= 'z')
+        return c - 'a';
+    // Großbuchstaben (ohne Umlaute)
+    else if (c >= 'A' && c <= 'Z')
+        return c - 'A';
+    // alles andere
+    else
+        return -1;
+}
+StreamReader sr = new StreamReader(@"..\kafka_verwandlung.txt");
+string line, longestWord = "";
+int[] occurences = new int[26];
+int[] kombinationen = new int[676]; //26*26 Variationen aa,ab,ac,ad,...,zx,zy,zz. 1. buchstabe = index / 26 2. buchstabe = index % 26
+int symbolcount = 0, wordcount = 0, linecount = 0;
+int[,] mostusedcomb = new int[2,26];
+while (!sr.EndOfStream)
 {
     line = sr.ReadLine()!;
-    if(line!="")
+    if (line != "")
     {
-        linecount ++;
+        linecount++;
         string[] words = line.Split(" ");
-        longestWord = LongestWord(words,longestWord);
+        longestWord = LongestWord(words, longestWord);
         wordcount += words.Length;
-        symbolcount += line.Length;
+        for (int i = 0; i < line.Length; i++)
+        {
+            symbolcount++;
+            if (Index(line[i]) != -1)
+            {
+                occurences[Index(line[i])]++;
+                if (i < line.Length - 1)
+                    if (Index(line[i + 1]) != -1)
+                    {
+                        kombinationen[(Index(line[i]) * 26) + Index(line[i + 1])]++;
+                        if (kombinationen[(Index(line[i]) * 26) + Index(line[i + 1])] > mostusedcomb[1,Index(line[i])])
+                        {
+                            mostusedcomb[1,Index(line[i])] = kombinationen[(Index(line[i]) * 26) + Index(line[i + 1])]; //häufigkeit der Kombination
+                            mostusedcomb[0,Index(line[i])] = (Index(line[i]) * 26) + Index(line[i + 1]); //index der Kombination
+                        }
+                    }
+            }
+        }
+
     }
 }
 Console.WriteLine($"Zeilen: {linecount}");
 Console.WriteLine($"Wörter: {wordcount}");
 Console.WriteLine($"Zeichen: {symbolcount}");
 Console.WriteLine($"Längstes Wort: {longestWord}");
+for (int i = 'a'; i <= 'z'; i++)
+{
+    Console.WriteLine($"{(char)i}:{occurences[i - 'a'],6}; Häufigste Kombination: {(char)(mostusedcomb[0,i-'a']/26+'a')}{(char)(mostusedcomb[0,i-'a']%26+'a')} ({mostusedcomb[1,i-'a']}X)");
+}
--- Aufgabe4_TextAnalyse/TextStatistikSchnipsel.cs
+++ Aufgabe4_TextAnalyse/TextStatistikSchnipsel.cs
@@ -15,26 +15,6 @@
                 return -1;
         }
 
-        static void Main(string[] args)
-        {
-            // Längeres Beispiel
-            string[] lines = {
-                "Alan Mathison Turing (1912-1954) war ein britischer Logiker,",
-                "Mathematiker, Kryptoanalytiker und Informatiker.",
-                "Das von ihm entwickelte Berechenbarkeitsmodell der Turingmaschine ",
-                "bildet eines der Fundamente der Theoretischen Informatik.",
-                "Während des Zweiten Weltkrieges war er maßgeblich an der Entzifferung",
-                "der mit der deutschen Rotor-Chiffriermaschine Enigma verschlüsselten deutschen Funksprüche beteiligt.",
-                "Nach ihm benannt sind der Turing Award, die bedeutendste Auszeichnung in der Informatik,",
-                "sowie der Turing-Test zum Überprüfen des Vorhandenseins von künstlicher Intelligenz.",
-            };
-            // Kurzes Beispiel
-            //string[] lines = { "Guten Tag", 
-            //                   "und guten Morgen" };
 
-            // Demo für Index-Funktion
-            foreach (char c in "abc,xyz")
-                Console.WriteLine($"{c}: {Index(c)}");
-        }
     }
 }

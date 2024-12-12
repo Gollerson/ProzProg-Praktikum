c27315c45bec63331cf4a300c53de33defd70a6f
marius goller <miron@goller24.de>
2024-12-12 13:33:19 +0100

    Rep angelegt

---
+++ Aufgabe4_TextAnalyse/TextAnalyse/Program.cs
@@ -0,0 +1,29 @@
+﻿static string LongestWord(string[] words,string longestWord) //gets all words in a Line and the prev longest word, returns longest word so far
+{
+    string longest = longestWord;
+    for(int i = 0; i<words.Length;i++)
+    {
+        if(words[i].Length > longest.Length)
+            longest = words[i];
+    }
+    return longest; 
+}
+StreamReader sr = new StreamReader(@"..\Test.txt");
+string line,longestWord = "";
+int symbolcount = 0,wordcount = 0,linecount = 0;
+while(!sr.EndOfStream)
+{
+    line = sr.ReadLine()!;
+    if(line!="")
+    {
+        linecount ++;
+        string[] words = line.Split(" ");
+        longestWord = LongestWord(words,longestWord);
+        wordcount += words.Length;
+        symbolcount += line.Length;
+    }
+}
+Console.WriteLine($"Zeilen: {linecount}");
+Console.WriteLine($"Wörter: {wordcount}");
+Console.WriteLine($"Zeichen: {symbolcount}");
+Console.WriteLine($"Längstes Wort: {longestWord}");

45397612b59730586f9c598349432309817aa5c6
Gollerson <miron@goller24.de>
2024-12-15 14:18:41 +0100

    Update Program.cs

--- Aufgabe4_TextAnalyse/TextAnalyse/Program.cs
+++ Aufgabe4_TextAnalyse/TextAnalyse/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-static string LongestWord(string[] words, string longestWord) //gets all words in a Line and the prev longest word, returns longest word so far
+static string LongestWord(string[] words, string longestWord) // Checkt ob in einem Array von wörtern das jetzt längste wort ist und gibt das zurück.
 {
     string longest = longestWord;
     for (int i = 0; i < words.Length; i++)
@@ -36,17 +36,17 @@ while (!sr.EndOfStream)
         string[] words = line.Split(" ");
         longestWord = LongestWord(words, longestWord);
         wordcount += words.Length;
-        for (int i = 0; i < line.Length; i++)
+        for (int i = 0; i < line.Length; i++) //Iteriere durch alle Symbole
         {
             symbolcount++;
-            if (Index(line[i]) != -1)
+            if (Index(line[i]) != -1) // Nur wenn das Zeichen ein Buchstabe ist (wichtig für die Kombinationen)
             {
-                occurences[Index(line[i])]++;
+                occurences[Index(line[i])]++; //Zähle alle Instanzen eines buchstaben mit um die Häufigkeit des Buchstaben zu erhalten
                 if (i < line.Length - 1)
                     if (Index(line[i + 1]) != -1)
                     {
-                        kombinationen[(Index(line[i]) * 26) + Index(line[i + 1])]++;
-                        if (kombinationen[(Index(line[i]) * 26) + Index(line[i + 1])] > mostusedcomb[1,Index(line[i])])
+                        kombinationen[(Index(line[i]) * 26) + Index(line[i + 1])]++; //adde 2 stellige kombination nach array logik von aa,ab,ac,...ax,ay,az,ba,bb,bc,...yx,zy,zz
+                        if (kombinationen[(Index(line[i]) * 26) + Index(line[i + 1])] > mostusedcomb[1,Index(line[i])]) // wenn die kombination am häufigsten ist -> update die meistverwendetste Kombination
                         {
                             mostusedcomb[1,Index(line[i])] = kombinationen[(Index(line[i]) * 26) + Index(line[i + 1])]; //häufigkeit der Kombination
                             mostusedcomb[0,Index(line[i])] = (Index(line[i]) * 26) + Index(line[i + 1]); //index der Kombination
@@ -63,5 +63,5 @@ Console.WriteLine($"Zeichen: {symbolcount}");
 Console.WriteLine($"Längstes Wort: {longestWord}");
 for (int i = 'a'; i <= 'z'; i++)
 {
-    Console.WriteLine($"{(char)i}:{occurences[i - 'a'],6}; Häufigste Kombination: {(char)(mostusedcomb[0,i-'a']/26+'a')}{(char)(mostusedcomb[0,i-'a']%26+'a')} ({mostusedcomb[1,i-'a']}X)");
+    Console.WriteLine($"{(char)i}:{occurences[i - 'a'],6}; Häufigste Kombination: {(char)(mostusedcomb[0,i-'a']/26+'a')}{(char)(mostusedcomb[0,i-'a']%26+'a')} ({mostusedcomb[1,i-'a']}X)"); //Ganze Ausgabe in eine Zeile gequetscht. Hatte aber kein Bock das aufzusplitten.
 }
